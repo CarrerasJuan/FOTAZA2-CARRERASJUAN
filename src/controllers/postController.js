c910a4938e4a89fa2d24d09fd4d74249fde8e3f3
CarrerasJE <148992169+CarrerasJuan@users.noreply.github.com>
2026-05-20 15:15:44 -0300

    Agregue listado básico de publicaciones

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,8 +1,30 @@
-const index = (req, res) => {
-    res.status(200).json({
-        module: "posts",
-        status: "ok"
-    });
+const { Post, User, Media } = require("../models");
+
+const index = async (req, res, next) => {
+    try {
+        const posts = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    as: "user",
+                    attributes: ["id", "username", "avatar_url"]
+                },
+                {
+                    model: Media,
+                    as: "media",
+                    attributes: ["id", "type", "url", "license", "watermark_text", "created_at"]
+                }
+            ],
+            order: [["created_at", "DESC"]]
+        });
+
+        return res.status(200).render("posts/index", {
+            title: "Publicaciones",
+            posts
+        });
+    } catch (error) {
+        return next(error);
+    }
 };
 
 module.exports = {
