ce4255517933c5d10abe86b0962d044ba0144947
CarrerasJE <148992169+CarrerasJuan@users.noreply.github.com>
2026-06-08 19:46:23 -0300

    debug: capturar error de subida avatar

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -186,13 +186,6 @@ const update = async (req, res, next) => {
         let finalStoragePath = null;
 
         if (req.file) {
-            return res.json({
-                debug: "req.file existe",
-                fileName: req.file.originalname,
-                mimeType: req.file.mimetype,
-                size: req.file.size
-            });
-
             if (!isSupabaseStorageConfigured()) {
                 return renderEditForm(res, "La subida de avatar requiere configurar Supabase Storage en el servidor.", {
                     username,
@@ -202,10 +195,18 @@ const update = async (req, res, next) => {
                 });
             }
 
-            uploadedAvatar = await uploadAvatar({
-                file: req.file,
-                userId: profileUser.id
-            });
+            try {
+                uploadedAvatar = await uploadAvatar({
+                    file: req.file,
+                    userId: profileUser.id
+                });
+            } catch (uploadError) {
+                return res.json({
+                    debug: "Error al subir avatar",
+                    errorMessage: uploadError.message,
+                    errorStack: uploadError.stack
+                });
+            }
 
             finalAvatarUrl = uploadedAvatar.publicUrl;
             finalStoragePath = uploadedAvatar.storagePath;
